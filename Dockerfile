fdc4eec1fdd7bcfb4ceb9cfecd07a5e2925b6c3c
Denys Chornokon <denyschornokon3@gmail.com>
2023-12-27 00:54:57 +0200

    task1 start

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM python:3
-ADD main.py /
-RUN pip install flask
-CMD [ "python", "main.py" ]
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,64 +1,52 @@
 terraform {
+  required_version = ">=0.13.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.28.0"
+      version = "~> 3.0"
     }
   }
 }
 
-# Configure the AWS provider
 provider "aws" {
-  region     = "eu-central-1"
+  region = "eu-central-1"
 }
 
-variable "REPOSITORY_URI" {
-  type = string
-}
-
-resource "aws_lightsail_container_service" "flask_application" {
-  name = "flask-app"
-  power = "nano"
-  scale = 1
-
-  private_registry_access {
-    ecr_image_puller_role {
-      is_active = true
-    }
+resource "aus_security_group" "web_app" {
+  name = "web_app"
+  description = "security group"
+  ingress {
+    grom_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-
+  ingress {
+    grom_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  engress {
+    grom_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
-    version = "1.0.0"
+    Name = "web_app"
   }
 }
 
-resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
-  container {
-    container_name = "flask-application"
-
-    image = "${var.REPOSITORY_URI}:latest"
-
-    ports = {
-      # Consistent with the port exposed by the Dockerfile and app.py
-      8080 = "HTTP"
-    }
+resource "aws_instance" "webapp_instance" {
+  ami = "ami-0669b163befffbdfc"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "webapp_instance"
   }
+}
 
-  public_endpoint {
-    container_name = "flask-application"
-    # Consistent with the port exposed by the Dockerfile and app.py
-    container_port = 8080
-
-    health_check {
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-      timeout_seconds     = 2
-      interval_seconds    = 5
-      path                = "/"
-      success_codes       = "200-499"
-    }
-  }
-
-  service_name = aws_lightsail_container_service.flask_application.name
+output "instance_public_ip" {
+  value = aws_instance.webapp_instance.public_ip
+  sensitive = true
 }
